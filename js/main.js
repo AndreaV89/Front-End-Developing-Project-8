c379a600f9e8fd3a1307303594b03431b5785161
AndreaV <a.vannetti08@gmail.com>
2020-01-12 18:58:45 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const container = document.getElementById('employee-container');
+const modalContainer = document.getElementById('modal-container');
 const $search = $('#search');
-const cards = document.getElementsByClassName('card');
+const $cards = document.getElementsByClassName('card');
 
 // Fetch Employee
 
 fetch('https://randomuser.me/api/?results=12&nat=us')
     .then(response => response.json())
     .then(data => {
-        console.log(data.results);
+        // console.log(data.results);
         createArray(data.results);
         generateHTML(employeeArray);
     })
@@ -67,8 +68,77 @@ $search.on('keyup', function(){
 });
 
 // Create Modal 
+let index;
+$('document').ready(function() {
+    $('#employee-container').on('click', '.card', function(){
+        index = $(this).index();
+        generateModalHTML(index);
+        $('#modal-container').css('display', 'block');
+        return index;
+    });
+})
+
+// Create Modal Card
 
 let modalHTML = "";
-container.on('click', function(){
-    alert("create modal");
-});
+function generateModalHTML(index) {
+    modalHTML += '<div id="cardContainer">';
+    modalHTML += '<span id="leftArrow"><</span>';
+    modalHTML += '<div id="selectedCard">';
+    modalHTML += '<p id="exitModal">X</p>';
+    modalHTML += `<img src="${employeeArray[index].image}" alt="Profile Image">`;
+    modalHTML += '<div class="selectedCard-text">';
+    modalHTML += `<h2>${employeeArray[index].firstName} ${employeeArray[index].lastName}</h2>`;
+    modalHTML += `<p>${employeeArray[index].email}</p>`;
+    modalHTML += `<p>${employeeArray[index].city}</p>`;
+    modalHTML += '<div id="modalLine"></div>';
+    modalHTML += `<p>${employeeArray[index].phone}</p>`;
+    modalHTML += `<p>${employeeArray[index].streetNumber} ${employeeArray[index].streetName}, ${employeeArray[index].postcode}</p>`;
+    modalHTML += `<p>Birthday: ${employeeArray[index].birthdate}</p>`;
+    modalHTML += '</div>';
+    modalHTML += '</div>';
+    modalHTML += '<span id="rightArrow">></span>';
+    modalHTML += '</div>';
+    modalContainer.innerHTML = modalHTML;
+    if(index === 0) {
+        $('#leftArrow').css('visibility', 'hidden');
+    } else {
+        $('#leftArrow').css('visibility', 'visible');
+    }
+    if(index === 11) {
+        $('#rightArrow').css('visibility', 'hidden');
+    } else {
+        $('#rightArrow').css('visibility', 'visible');
+    }
+}
+
+// Exit Modal 
+
+$('document').ready(function() {
+    $('#modal-container').on('click', '#exitModal', function() {
+        modalHTML = "";
+        $('#cardContainer').remove();
+        $('#modal-container').css('display', 'none');
+    })
+})
+
+// Modal Arrow 
+
+$('document').ready(function() {
+    $('#modal-container').on('click', '#leftArrow', function() {
+        index -= 1;
+        modalHTML = "";
+        $('#cardContainer').remove();
+        generateModalHTML(index);
+    })
+})
+
+$('document').ready(function() {
+    $('#modal-container').on('click', '#rightArrow', function() {
+        index += 1;
+        modalHTML = "";
+        $('#cardContainer').remove();
+        generateModalHTML(index);
+    })
+})
+
